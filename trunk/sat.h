3dcb2a7f8c8e5a0c30716e2e0ea94be582bc1620
mateifl1@gmail.com <mateifl1@gmail.com@f606a6dc-dbc2-aab6-d858-7349e709d34f>
2013-10-19 19:52:15 +0000

    Added 2-SAT problem

--- trunk/sat.h
+++ trunk/sat.h
@@ -5,7 +5,10 @@ struct condition {
     int t1;
     int t2;
 };
+
 vector<condition> read_sat_data(string filename, set<int> &numbers);
 typedef struct condition condition;
 bool check_condition(const condition & cond, const vector<bool> numbers);
+void preprocess( vector<condition> & conditions, set<int> & numbers );
+
 #endif // SAT_H_INCLUDED
