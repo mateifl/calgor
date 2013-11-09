c28df369e0dad12b44ac097fff7ef079af288f75
mateifl1 <mateifl1@gmail.com>
2013-11-09 22:39:50 +0000

    added code for hmro problem (spoj)

--- main.cpp
+++ main.cpp
@@ -1,38 +1,32 @@
-#include "ppath.h"
+#include "hmro.h"
 
-int main(int argc, char* argv[])
+int main(int argc, char **argv)
 {
-    clock_t c1 = clock();
-    set<int> primes = prime_numbers(1000, 10000);
-    clock_t c2 = clock() - c1;
-    cout << "primes: " << ((float)c2)/CLOCKS_PER_SEC << endl;
-    graph g = create_graph(primes);
-    clock_t c3 = clock() - c2;
-    cout << "graph: " << ((float)c3)/CLOCKS_PER_SEC << endl;
-    int path_len = bfs(g, 1033, 8179);
-    cout << "bfs: " << path_len << endl;
-    clock_t c4 = clock() - c3;
+    string_map s;
+    /*
+    s.insert( make_pair("test1", "test2") );
+    s.insert( make_pair("test2", "test3") );
+    s.insert( make_pair("test3", "test4") );
+    s.insert( make_pair("test5", "test6") );
+    s.insert( make_pair("test6", "test7") );
+    s.insert( make_pair("test_key4", "test_value4") );
     
-    for(int i = 1; i < 20; i++)
-    {
-        path_len = bfs(g, 1373, 8017);
-        cout << "bfs: " << path_len << endl;
-
-        path_len = bfs(g, 1049, 8089);
-        cout << "bfs: " << path_len << endl;
-
-        path_len = bfs(g, 7103, 4441);
-        cout << "bfs: " << path_len << endl;
-
-        path_len = bfs(g, 8243, 1013);
-        cout << "bfs: " << path_len << endl;
-
-        path_len = bfs(g, 6569, 7211);
-        cout << "bfs: " << path_len << endl;
+    string_map s1 = new_centers(s);
+    
+    string_map::iterator it;
+    
+    for( it = s1.begin(); it != s1.end(); it++ ) {
+        cout << it->first << " " << it->second << endl;
     }
-    c4 = clock() - c4;
-    cout << "Total calls: " << ((float)c4)/CLOCKS_PER_SEC << endl;
-
+     */
+    FILE *f;
+    f = fopen("hmro_tc.txt", "r");
+    s = read_pairs(f, 11, 5);
+    cout << s.size() << endl;
+    string_map centers = read_pairs(f, 5, 5);
+    cout << centers.size() << endl;
+    fclose(f);
+    string_map new_c = new_centers(centers);
+        
     return 0;
 }
-
