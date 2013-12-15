81db431451df0a7f78e296856ac43d4d639b05e5
mateifl1 <mateifl1@gmail.com>
2013-12-15 21:14:33 +0000

    created template function for subsets

--- pop1.cpp
+++ pop1.cpp
@@ -48,11 +48,11 @@ template <typename T> bool check_if_prime(T l_number, vector<T> &primes, set<T> 
 	if( (l_number - 1) % 6 != 0 && (l_number + 1) % 6 != 0 )
 		return false;
 
-	if( prime_set.find( l_number ) != prime_set.end() )
+	if( l_number < 35000LL  )
 	{
-		//cout << "hit" << endl; 
-		return true;
+		return (prime_set.find( l_number ) != prime_set.end());
 	}
+
     T sqr =  sqrt( ( long double )l_number ) + 1;
 	typename vector<T>::iterator it;
 	for(it = primes.begin(); it != primes.end(); it++){
@@ -73,18 +73,18 @@ bool check_number2( long long number, vector<long long> &primes, set<long long> 
 	
 	while(i < strlen(pch_number)) {
 		i_copy_lenght = strlen(pch_number) - i;
-		strncpy(pch_buffer, pch_number, i_copy_lenght);
-		pch_buffer[i_copy_lenght] = '\0';
+		strncpy(pch_buffer, pch_number, i);
+		pch_buffer[i] = '\0';
 		n = atol(pch_buffer);
-
+		//cout << "n = " << n << endl;
 		if( !check_if_prime<long long>(n, primes, primes_set) )
 		{
 			i++;
 			continue;
 		}
-
-		strncpy(pch_buffer, pch_number + i_copy_lenght, i);
-		pch_buffer[i + 1] = '\0';
+		//cout << "n is prime" << endl;
+		strncpy(pch_buffer, pch_number + i, i_copy_lenght);
+		pch_buffer[i_copy_lenght] = '\0';
 		// leading zeros not allowed
 
 		if(pch_buffer[0] == '0')
@@ -92,25 +92,16 @@ bool check_number2( long long number, vector<long long> &primes, set<long long> 
 			i++;
 			continue;
 		}
-		rest = atol(pch_buffer);		
+		rest = atol(pch_buffer);	
+		//cout << "rest = " << rest << endl;	
 		//cout << n << " " << rest << endl;
 
-		// if the first part is string, check the last
-		if(check_if_prime<long long>(rest, primes, primes_set)){
-		// if it is, number can be split, so return true
-		//		cout << "Solution: " << n << " " << rest << endl;				
-			return true;
-		}
+		bool res = check_number2(rest, primes, primes_set);
 
-		//	cout << "recurse on " << rest << endl;
-		bool result = check_number2(rest, primes, primes_set);
+		if(res)
+			return res;
 
-		if(result)
-			return result;
-		else{
-			i++;
-			continue;
-		}
+		return check_if_prime(rest, primes, primes_set);
 	}
 	return false;
 }
@@ -118,19 +109,22 @@ bool check_number2( long long number, vector<long long> &primes, set<long long> 
 int main(int argc, char** argv) {
 	long i_nr_tests;
     long long nr;
-	scanf("%d\n", &i_nr_tests);
+	scanf("%ld\n", &i_nr_tests);
     char pch_nr[12];
     clock_t t1 = clock();
-	vector<long long> primes = prime_numbers<long long>(2LL, 40000LL);
+	vector<long long> primes = prime_numbers<long long>(2LL, 35000LL);
 	vector<long long>::iterator it; 
 	set<long long> primes_set; 
 
-	for(it = primes.begin(); it != primes.end(); it++)
+	for(it = primes.begin(); it != primes.end(); it++) {
 		primes_set.insert(*it);
+	}
 
     clock_t t2 = clock();
     cout << "Primes: " << (float)(t2 - t1)/CLOCKS_PER_SEC << endl;
 	
+    cout << i_nr_tests << endl;
+
 	for(int i = 0; i < i_nr_tests; i++) {
         fgets(pch_nr, 11, stdin);			
         nr = atol(pch_nr);
