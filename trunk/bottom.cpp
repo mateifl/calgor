e7e0e740373815ed4d9cd3460964b1dce47ca8e2
mateifl1@gmail.com <mateifl1@gmail.com@f606a6dc-dbc2-aab6-d858-7349e709d34f>
2013-12-21 22:59:17 +0000

    ...

--- trunk/bottom.cpp
+++ trunk/bottom.cpp
@@ -9,32 +9,31 @@
 	processed_node - map with the status of every node: 0 - not visited, 1 - visited (node visited but not every neighbour), 
 	2 - processed (the node and everyone of it's negihbours have been visited).
 	visitor - object used to gather data related to the DFS.
-*/
+ */
 template <typename T> void generic_dfs(map<T, vector<T> > g, T start_node, map<T, short> &processed_nodes, dfs_visitor<T> *visitor) {
 	stack<T> node_stack;
-    node_stack.push(start_node);
+	node_stack.push(start_node);
 	T node;
 
 	vector<T> neighbors;
-    typename vector<T>::iterator it;
+	typename vector<T>::iterator it;
 	vector<T> nodes;
 
-    while(node_stack.size() > 0){
+	while (node_stack.size() > 0) {
 		nodes.clear();
-        node = node_stack.top();
+		node = node_stack.top();
 		// mark node as discovered
 		processed_nodes[node] = 1;
 		// get the neighbours
-        neighbors = g[node];
+		neighbors = g[node];
 		bool continue_flag = false;
 
-		for(it = neighbors.begin(); it != neighbors.end(); it++) {
+		for (it = neighbors.begin(); it != neighbors.end(); it++) {
 			short node_state = processed_nodes[*it];
 
-			if( node_state == 2 )
+			if (node_state == 2)
 				continue;
-			else if(node_state == 0)
-			{
+			else if (node_state == 0) {
 				processed_nodes[*it] = 1;
 				node_stack.push(*it);
 				continue_flag = true;
@@ -42,7 +41,7 @@ template <typename T> void generic_dfs(map<T, vector<T> > g, T start_node, map<T
 			}
 		}
 
-		if(continue_flag)
+		if (continue_flag)
 			continue;
 		// node processed
 		processed_nodes[node] = 2;
@@ -50,85 +49,82 @@ template <typename T> void generic_dfs(map<T, vector<T> > g, T start_node, map<T
 		nodes.push_back(node);
 		nodes.push_back(start_node);
 		visitor->visit(nodes);
-    }
+	}
 }
 
 void read_edge_by_line(FILE *f, graph &g, graph &g_reversed) {
 	int i_number_vertices, i_number_of_edges, i_start_vertex, i_end_vertex;
-	char pch_line[128], *pch;// = new char[16];
+	char pch_line[128], *pch; // = new char[16];
 	fscanf(f, "%d %d\n", &i_number_vertices, &i_number_of_edges);
-	for(int i = 0; i < i_number_of_edges; i++)
-	{
+	for (int i = 0; i < i_number_of_edges; i++) {
 		fgets(pch_line, 128, f);
 		pch = strtok(pch_line, " ");
 		i_start_vertex = atol(pch);
-        pch = strtok(NULL, " \n");
-        i_end_vertex = atol(pch);
-        g[i_start_vertex].push_back(i_end_vertex);
-        g_reversed[i_end_vertex].push_back(i_start_vertex);
+		pch = strtok(NULL, " \n");
+		i_end_vertex = atol(pch);
+		g[i_start_vertex].push_back(i_end_vertex);
+		g_reversed[i_end_vertex].push_back(i_start_vertex);
 	}
 }
 
-
 void read_edge_by_line(FILE *f, graph2 &g, graph2 &g_reversed) {
 	int i_number_vertices, i_number_of_edges, j;
-	char pch_line[128], *pch;// = new char[16];
+	char pch_line[128], *pch; // = new char[16];
 	fscanf(f, "%d %d\n", &i_number_vertices, &i_number_of_edges);
-    vector<int> data(2 * i_number_of_edges);
+	vector<int> data(2 * i_number_of_edges);
 
-	for(int i = 0; i < 2 * i_number_of_edges; i += 2)
-	{
+	for (int i = 0; i < 2 * i_number_of_edges; i += 2) {
 		fgets(pch_line, 128, f);
 		pch = strtok(pch_line, " ");
 		j = atol(pch);
-        data[i] = j;
-        pch = strtok(NULL, " \n");
-        j = atol(pch);
-        data[i + 1] = j;
+		data[i] = j;
+		pch = strtok(NULL, " \n");
+		j = atol(pch);
+		data[i + 1] = j;
 	}
 
-    int size_vector = *max_element( data.begin(), data.end() );
+	int size_vector = *max_element(data.begin(), data.end());
 
-    g = graph2(size_vector + 1);
-    g_reversed = graph2(size_vector + 1);
-    
-	for(int i = 0; i < 2 * i_number_of_edges; i += 2) {
-        if( g[data[i]] == NULL )
-            g[data[i]] = new vector<int>();
+	g = graph2(size_vector + 1);
+	g_reversed = graph2(size_vector + 1);
 
-		if( g[data[i + 1]] == NULL )
-            g[data[i + 1]] = new vector<int>();
+	for (int i = 0; i < 2 * i_number_of_edges; i += 2) {
+		if (g[data[i]] == NULL)
+			g[data[i]] = new vector<int>();
 
-        g[data[i]]->push_back(data[i + 1]);
+		if (g[data[i + 1]] == NULL)
+			g[data[i + 1]] = new vector<int>();
 
-        if( g_reversed[data[i + 1]] == NULL )
-            g_reversed[data[i + 1]] = new vector<int>();
+		g[data[i]]->push_back(data[i + 1]);
 
-        if( g_reversed[data[i]] == NULL )
-            g_reversed[data[i]] = new vector<int>();
+		if (g_reversed[data[i + 1]] == NULL)
+			g_reversed[data[i + 1]] = new vector<int>();
 
-        g_reversed[data[i + 1]]->push_back(data[i]);
-    }
+		if (g_reversed[data[i]] == NULL)
+			g_reversed[data[i]] = new vector<int>();
+
+		g_reversed[data[i + 1]]->push_back(data[i]);
+	}
 }
 
 void read_data(FILE *f, graph &g, graph &g_reversed) {
 
-    int i_number_vertices, i_number_of_edges, i_start_vertex, i_end_vertex;
-    char pch_line[512 * 100], *pch = new char[16];
-    fscanf(f, "%d %d\n", &i_number_vertices, &i_number_of_edges);
-    fgets(pch_line, 512 * 100, f);
+	int i_number_vertices, i_number_of_edges, i_start_vertex, i_end_vertex;
+	char pch_line[512 * 100], *pch = new char[16];
+	fscanf(f, "%d %d\n", &i_number_vertices, &i_number_of_edges);
+	fgets(pch_line, 512 * 100, f);
 
-    pch = strtok(pch_line, " ");
-    while( pch != NULL ){
-        i_start_vertex = atol(pch);
-        pch = strtok(NULL, " ");
-        if( pch == NULL )
-            break;
-        i_end_vertex = atol(pch);
-        g[i_start_vertex].push_back(i_end_vertex);
-        g_reversed[i_end_vertex].push_back(i_start_vertex);
-        pch = strtok(NULL, " ");
-    }
+	pch = strtok(pch_line, " ");
+	while (pch != NULL) {
+		i_start_vertex = atol(pch);
+		pch = strtok(NULL, " ");
+		if (pch == NULL)
+			break;
+		i_end_vertex = atol(pch);
+		g[i_start_vertex].push_back(i_end_vertex);
+		g_reversed[i_end_vertex].push_back(i_start_vertex);
+		pch = strtok(NULL, " ");
+	}
 }
 /*
 int main(int argc, char** argv) {
@@ -137,20 +133,20 @@ int main(int argc, char** argv) {
 	clock_t t1 = clock();
 	f = fopen("edges5000_1_res_70.txt", "r");
 	read_edge_by_line(f, g, g_rev);
-    clock_t t2 = clock();
-    cout << "Read time: " << (float)(t2 - t1)/CLOCKS_PER_SEC << endl;
+	clock_t t2 = clock();
+	cout << "Read time: " << (float)(t2 - t1)/CLOCKS_PER_SEC << endl;
 	map<int, int> leaders = calculate_sccs<graph2, int>(g, g_rev);
     
 	map<int, int>::iterator it;
-    map<int, set<int> > sccs_groups;
-    clock_t t3 = clock();
-    cout << "sccs time: " << (float)(t3 - t2)/CLOCKS_PER_SEC << endl;
+	map<int, set<int> > sccs_groups;
+	clock_t t3 = clock();
+	cout << "sccs time: " << (float)(t3 - t2)/CLOCKS_PER_SEC << endl;
 	for(it = leaders.begin(); it != leaders.end();  it++) 
-        sccs_groups[it->second].insert(it->first);
+		sccs_groups[it->second].insert(it->first);
     
-    clock_t t4 = clock();
-    cout << "Arrange time: " << (float)(t4 - t3)/CLOCKS_PER_SEC << endl;
-    cout << sccs_groups.size() << endl;
+	clock_t t4 = clock();
+	cout << "Arrange time: " << (float)(t4 - t3)/CLOCKS_PER_SEC << endl;
+	cout << sccs_groups.size() << endl;
 
 	map<int, set<int> >::iterator it_sccs;
 	set<int>::iterator it_group;
@@ -169,6 +165,6 @@ int main(int argc, char** argv) {
 		}
 	}
 
-    return 0;
+	return 0;
 }
-*/
+ */
