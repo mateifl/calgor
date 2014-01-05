7d14492b30fd8616d2ac4eb5b85e482867238565
mateifl1@gmail.com <mateifl1@gmail.com@f606a6dc-dbc2-aab6-d858-7349e709d34f>
2014-01-05 22:01:38 +0000

    Added code for spoj HERDING

---
+++ trunk/herding.cpp
@@ -0,0 +1,33 @@
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+typedef vector<vector<int> > graph;
+
+vector<vector<char> > read_data() {
+    vector<vector<char> > res;
+    return res;
+}
+
+graph create(vector<vector<char> > m, int rows, int columns ) {
+    graph g = vector<vector<int> >(rows * columns);
+    for( int i = 0; i < rows; i++ )
+    {
+        for( int j = 0; j < columns; j++ )
+        {
+            
+        }
+    }
+    
+}
+
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
+
