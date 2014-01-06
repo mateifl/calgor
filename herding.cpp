b01c800fb0f620a7c533367689286e71ebe5828f
mateifl1 <mateifl1@gmail.com>
2014-01-06 21:29:49 +0000

    work on herding

--- herding.cpp
+++ herding.cpp
@@ -5,7 +5,7 @@
 #include <queue>
 
 using namespace std;
-
+char coordinates[] = {'S', 'N', 'E', 'W'}; 
 typedef vector<vector<int> > graph;
 
 vector<vector<char> > read_data(FILE *f) {
@@ -13,14 +13,15 @@ vector<vector<char> > read_data(FILE *f) {
 	fscanf(f, "%d %d\n", &rows, &columns);
 	vector<char> v_line(columns);
 	vector<vector<char> > v_res(rows);
-	char pch_line[1024];
-	for (int i = 0; i < rows; i++)
+	char *pch_line = new char[columns + 1];
+	for (int i = 0; i <= rows; i++)
 	{
-		fgets(pch_line, 1001, f);
+		fgets(pch_line, columns + 1, f);
+		pch_line[columns] = '\0';
+		cout << pch_line << endl;
 		for (int j = 0; j < columns; j++)
-		{
 			v_line[j] = pch_line[j];
-		}
+		v_res[i] = v_line;
 	}
 
     return v_res;
@@ -28,11 +29,20 @@ vector<vector<char> > read_data(FILE *f) {
 
 graph create(vector<vector<char> > m, int rows, int columns ) {
     graph g = vector<vector<int> >(rows * columns);
+	int k = 0;
     for( int i = 0; i < rows; i++ )
     {
         for( int j = 0; j < columns; j++ )
         {
-            
+			if( m[i][j] == coordinates[0] )
+				g[k].push_back( k + columns);
+			else if( m[i][j] == coordinates[1] )
+				g[k].push_back( k - columns);
+			if( m[i][j] == coordinates[0] )
+				g[k].push_back( k + 1);
+			if( m[i][j] == coordinates[0] )
+				g[k].push_back( k - 1);
+			k++;
         }
     }
     
@@ -40,7 +50,9 @@ graph create(vector<vector<char> > m, int rows, int columns ) {
 
 
 int main(int argc, char** argv) {
-    
+    vector<vector<char> > data = read_data(stdin);
+    cout << data.size() << " " << data[0].size() << endl;
+    graph g = create(data, data.size(), data[0].size() );
     return 0;
 }
 
