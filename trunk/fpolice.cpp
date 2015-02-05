427418d622581f7457caf7b34246ac1069319652
mateifl1@gmail.com <mateifl1@gmail.com@f606a6dc-dbc2-aab6-d858-7349e709d34f>
2015-02-05 15:47:25 +0000

    added one more test case

--- trunk/fpolice.cpp
+++ trunk/fpolice.cpp
@@ -6,18 +6,26 @@
 #include <cstdlib>
 #include <cstdio>
 #include <climits>
+#include <ctime>
 using namespace std;
 
 typedef unsigned long ulong;
 
 typedef struct result
 {
+    public:
     int risk;
     int time;
+    result() { risk = -1; time = -1; }
 } result;
 
+result memo[251][101][101];
+
 result solution(int i_last_station, int i_steps, int i_time_limit,
-              vector< vector<ulong> > &times, vector< vector<ulong> > &risks) {
+				vector< vector<ulong> > &times, vector< vector<ulong> > &risks) {
+
+	if (memo[i_time_limit][i_last_station][i_steps].risk > -1)
+		return memo[i_time_limit][i_last_station][i_steps];
 
     result r;
     int i_no_locations = times.size();
@@ -33,17 +41,25 @@ result solution(int i_last_station, int i_steps, int i_time_limit,
         return r;
     }
 
-    r = solution(i_last_station, i_steps - 1, i_time_limit, times, risks);
+	r = memo[i_time_limit][i_last_station][i_steps - 1];
+	if (r.risk == -1)
+		r = solution(i_last_station, i_steps - 1, i_time_limit, times, risks);
 
     for(int i = 0; i < i_no_locations; i++) {
         if(i == i_last_station)
             continue;
 
         int s_tl = i_time_limit - times[i][i_last_station];
-        if(s_tl < 0)
+		if (s_tl < 0) {
+			//cout << "TL exceeded! " << i_last_station << " " << i_steps << endl;
             continue;
+		}
 
-        result r2 = solution(i, i_steps - 1, s_tl, times, risks);
+		result r2 = memo[s_tl][i][i_steps - 1];
+		if (r2.risk == -1) {
+			r2 = solution(i, i_steps - 1, s_tl, times, risks);
+			memo[s_tl][i][i_steps - 1] = r2;
+		}
 
         if (r.risk > r2.risk + risks[i][i_last_station]) {
             r.risk = r2.risk + risks[i][i_last_station];
@@ -51,13 +67,14 @@ result solution(int i_last_station, int i_steps, int i_time_limit,
         }
     }
 
-	printf("%d %d (%d %d)\n", i_last_station, i_steps, r.risk, r.time);
-
+	//printf("%d %d (%d %d)\n", i_last_station, i_steps, r.risk, r.time);
+	memo[i_time_limit][i_last_station][i_steps] = r;
     return r;
 }
 
 int main(int argc, char const *argv[])
 {
+	time_t t = clock();
 	int i_testcases, i_stations, i_time;
 	scanf("%d", &i_testcases);
 
@@ -66,20 +83,34 @@ int main(int argc, char const *argv[])
 
 		vector< vector<ulong> > times(i_stations);
         vector< vector<ulong> > risks(i_stations);
+		//result memo[251][101][101];
 		for(int j = 0; j < i_stations; j++) {
             times[j] = vector<ulong>(i_stations);
             risks[j] = vector<ulong>(i_stations);
         }
 
-        for(int j = 0; j < i_stations; j++)
-            for(int k = 0; k < i_stations; k++)
-                scanf("%ld", &times[j][k]);
+		for (int j = 0; j < i_stations; j++) {
+			for (int k = 0; k < i_stations; k++) {
+				scanf("%ld", &times[j][k]);
 
+			}
+		}
         for(int j = 0; j < i_stations; j++)
             for(int k = 0; k < i_stations; k++)
                 scanf("%ld", &risks[j][k]);
+
+		time_t t2 = clock();
         result r = solution(i_stations - 1, i_stations - 1, i_time, times, risks);
-        printf("%d %d\n", r.risk, r.time);
+
+		if (r.risk == -1 || r.risk == INT_MAX)
+			printf("-1\n");
+		else 
+			printf("%d %d\n", r.risk, r.time);
+
+		time_t t1 = clock();
+		cout << "time: " << (float)(t1 - t) / CLOCKS_PER_SEC << endl;
+		cout << "time: " << (float)(t2 - t) / CLOCKS_PER_SEC << endl;
 	}
+
     return 0;
 }
