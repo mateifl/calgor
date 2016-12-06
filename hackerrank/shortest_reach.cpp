c3df42faeb0a6a74813d28c8b0630d9099558c38
mateifl <mateifl@yahoo.com>
2016-12-06 11:08:05 +0200

    deleted some files

--- hackerrank/shortest_reach.cpp
+++ hackerrank/shortest_reach.cpp
@@ -14,43 +14,46 @@ typedef vector<vector<ulong> > graph;
 
 vector<ulong> bfs(graph g, ulong source_node) {
 
-	vector<bool> v_bool_visited = vector<bool>(g.size());
+	vector<bool> v_bool_visited = vector<bool>(g.size() + 1, false);
 	v_bool_visited[source_node] = true;
-    vector<ulong> m_shortest_path = vector<ulong>(g.size(), ULONG_MAX);
-	vector<ulong> m_shortest_path = vector<ulong>(g.size(), ULONG_MAX);
+    vector<ulong> m_shortest_path = vector<ulong>(g.size() + 1, ULONG_MAX);
 	m_shortest_path[source_node] = 0;
     queue<int> q;
 
 	for (ulong i = 0; i < g[source_node].size(); i++) {
 		q.push(g[source_node][i]);
 	}
-	
+
 	while (q.size() > 0) {
-		node = nodes_heap.top();
-		nodes_heap.pop();
+		ulong node = q.front();
+		q.pop();
+        cout << "Processing node: " << node << endl;
 
         vector<ulong> v_neighbors = g[node];
-
-		if (v_bool_visited[node])
-			continue;
-
+        for(ulong i = 0; i < v_neighbors.size(); i++)
+        {
+            if(!v_bool_visited[i]) {
+                q.push(v_neighbors[i]);
+                if(m_shortest_path[v_neighbors[i]] == ULONG_MAX)
+                    m_shortest_path[v_neighbors[i]] = m_shortest_path[node] + 1;
+            }
+        }
+        v_bool_visited[node] = true;
     }
 	return m_shortest_path;
 }
 
 int main(int argc, char **argv) {
-	FILE *ifile = stdin;
-    // FILE *ifile = fopen("input07.txt", "r");
-    
+	// FILE *ifile = stdin;
+    FILE *ifile = fopen("hackerrank/shortest_reach_tc2.txt", "r");
+
 	if (ifile == NULL) {
 		printf("ERROR no input file\n");
 		return 0;
 	}
 
-    ulong i_nodes, i_edges, i_start, i_end, i_head, i_tail, i_value, ul_testcases;
-	
-	char pch_line[64];
-	char *pch = new char[100];
+    ulong i_nodes, i_edges, i_start, i_end, i_head, i_tail, ul_testcases;
+
     fscanf(ifile, "%ld", &ul_testcases);
 	for(ulong k = 0; k < ul_testcases; k++)
 	{
@@ -60,26 +63,15 @@ int main(int argc, char **argv) {
         vector<vector<ulong> > edges = vector<vector<ulong> >(i_nodes + 1, vector<ulong>(i_nodes + 1, 0));
         // time_t t = clock();
         for (int j = 0; j < i_edges; j++){
-			fscanf(ifile, "%ld %ld %ld", &i_head, &i_tail, &i_value);
-            if(edges[i_head][i_tail] == 0) {
-                edges[i_head][i_tail] = i_value;
-                edges[i_tail][i_head] = i_value;
-            }
-            else if(edges[i_head][i_tail] > 0 && i_value < edges[i_head][i_tail]) {
-                edges[i_head][i_tail] = i_value;
-                edges[i_tail][i_head] = i_value;
-            }
+			fscanf(ifile, "%ld %ld", &i_head, &i_tail);
+            edges[i_head][i_tail] = 1;
         }
         // time_t t1 = clock();
         for (int j = 0; j <= i_nodes; j++){
             for (int k = 0; k <= i_nodes; k++){
                 if(edges[j][k] > 0)
                 {
-                    	edge e;
-                    	e.head = j;
-                    	e.tail = k;
-                    	e.value = edges[j][k];
-                    	g[j].push_back(e);
+                    g[j].push_back(k);
                 }
             }
         }
@@ -95,7 +87,6 @@ int main(int argc, char **argv) {
                 cout << -1 << " ";
             else
                 cout << v[i] << " ";
-            
         }
         cout << endl;
 	}
